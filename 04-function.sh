679b052e03fee21f21885bd47724a28afb13ce0b
Sumanth <9ssumanth@gmail.com>
2024-01-19 17:27:08 +0530

    function

--- 04-function.sh
+++ 04-function.sh
@@ -1,7 +1,19 @@
 #Function 
 sample () {
-"Hello World"
+echo "Hello World"
 }
+
+#Main program -- calling a function is called Main program
 sample
 
+#Declared variable in main program can be accessed in function and it's vice versa.
+#Declared variable in main program can be overwritten in function and it's vice versa.
+#Along with this function we can pass special variables, we can pass argument to function.
 
+#sample1 100 200
+
+sample_sum () {
+echo "Addition of $1 and $2 is $(($1+$2))"
+}
+
+sample_sum 100 200
