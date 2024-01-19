9750ad9cc7adc7fe9d311203bf11e3b99e006d58
Sumanth <9ssumanth@gmail.com>
2024-01-19 12:54:43 +0530

    Special variables along with argument types

--- 03-special-variable.sh
+++ 03-special-variable.sh
@@ -1,7 +1,7 @@
 # 0, 1 ,2, ... n, * ,#
 
 echo Name of the script $0
-echo First argument $1
-echo Second argument $2
-echo List of all arguments $*
-echo No. of arguments $#
+echo First argument - $1
+echo Second argument - $2
+echo List of all arguments - $*
+echo No. of arguments - $#
