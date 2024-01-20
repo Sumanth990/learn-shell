91ff33df2892aec99e1812cbfa9e9761c2921d1f
Sumanth <9ssumanth@gmail.com>
2024-01-20 18:52:04 +0530

    Set Exit command and return command status

--- 05-Exit.sh
+++ 05-Exit.sh
@@ -17,3 +17,4 @@ echo Hell
 #Any value between 0-255 can be used by scripting person, however values from 125+ (126 to 255) are been used by system.
 #So we usually don't use those values as author scripts and we are free to use from 0-255.
 
+
