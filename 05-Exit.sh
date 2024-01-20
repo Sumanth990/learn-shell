5435ec68217c3139c9882228f7ed9f4b0c7641c0
Sumanth <9ssumanth@gmail.com>
2024-01-20 18:51:37 +0530

    Exit command

--- 05-Exit.sh
+++ 05-Exit.sh
@@ -1,3 +1,19 @@
-echo Hello1
-exit
-echo hello2
+
+sample () {
+  echo Hello1
+  return 100
+  echo hello2
+}
+
+sample
+echo sample Exit status of function - $?
+
+echo Hello
+exit 200
+echo Hell
+
+
+
+#Any value between 0-255 can be used by scripting person, however values from 125+ (126 to 255) are been used by system.
+#So we usually don't use those values as author scripts and we are free to use from 0-255.
+
