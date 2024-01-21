5f27183759856cabf95183befb18a78c189f6e19
Sumanth <9ssumanth@gmail.com>
2024-01-21 22:35:26 +0530

    Break and continue

--- 07-loops.sh
+++ 07-loops.sh
@@ -29,4 +29,27 @@ done
 for course in DevOps AWS AZURE GCP Testing; do
  echo Welcome to course - ${course}
 done
+
+# Two loop control commands are there
+# break - When we want to stop a loop and come out of loop we use break.
+# continue - Skipping the remaining commands in loop (command1 ; comm2 ; continue; comm3 ; comm4) and starts the next iteration.
+
+#Break example
+i=10
+while [ "$i" -gt 0 ]; do
+ echo Hello world
+ i=$(($i-1))
+ break
+ echo Hello Universe
+done
+
+# Continue example
+
+i=10
+while [ "$i" -gt 0 ]; do
+ echo Hello World
+ i=$(($i-1))
+ continue
+ echo Hello Universe
+done
 ##################################
