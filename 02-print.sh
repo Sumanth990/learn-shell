a557409bfeda55e84507452fda3a2795884389a4
Sumanth <9ssumanth@gmail.com>
2024-05-05 08:41:20 +0530

    enable color code

--- 02-print.sh
+++ 02-print.sh
@@ -10,6 +10,13 @@ echo hello world
 #cyan - 36
 #reset - 0 black - 30 white -37
 
-syntax:
-echo -e "\e[32mHello world\e[0m"
-echo -e "\e[33mHello Hi\e[0m"
+#syntax:
+echo -e "/e[30mHello world/e[0m"
+echo -e "/e[31mHello world/e[0m"
+echo -e '/e[31mHello world/e[0m'
+echo -e "/e[32mHello world/e[0m"
+echo -e "/e[33mHello world/e[0m"
+echo -e "/e[34mHello world/e[0m"
+echo -e "/e[35mHello world/e[0m"
+echo -e "/e[36mHello world/e[0m"
+echo -e "/e[37mHello world/e[0m"
