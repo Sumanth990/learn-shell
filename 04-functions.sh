c7ecfdc92c47da8731fc362a2b83a381a0b4d6bf
Sumanth <9ssumanth@gmail.com>
2024-05-06 12:25:20 +0530

    creating sample function

--- 04-functions.sh
+++ 04-functions.sh
@@ -7,7 +7,7 @@ sample() {
 sample #calling a function is called main program
 
 sample_sum() {
-  echo Addition of $1 and $2 is $(($1+$2))
+  echo "Addition of $1 and $2 is $(($1+$2))"
 }
 
 sample_sum
