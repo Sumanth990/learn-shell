4756a626562ad426e39e618ccf268e768f2d4115
Sumanth <9ssumanth@gmail.com>
2024-05-06 13:24:10 +0530

    Exit command with return status value

--- 05-exit.sh
+++ 05-exit.sh
@@ -8,11 +8,20 @@ sample2() {
 sample2
 
 #Exit
-
 sample3() {
   echo "Hii"
   exit
   echo hello
+}
+
+sample3
+
+#return with 126
+
+sample3() {
+  echo hiii
+  return 127
+  echo he
 }
 
 sample3
