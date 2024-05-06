b4e7a9081941610fc4c2bb589309040207423871
Sumanth <9ssumanth@gmail.com>
2024-05-06 12:23:31 +0530

    creating sample function

--- 04-functions.sh
+++ 04-functions.sh
@@ -2,7 +2,12 @@
 
 sample() {
   echo "Hello world"
-  a=10
 }
 
-sample $a #calling a function is called main program
+sample #calling a function is called main program
+
+sample_sum() {
+  echo Addition of $1 and $2 is $(($1+$2))
+}
+
+sample_sum
