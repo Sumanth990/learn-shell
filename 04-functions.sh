1a414880d3f4adc9adfd5a64017749387ce96e7e
Sumanth <9ssumanth@gmail.com>
2024-05-06 12:36:32 +0530

    creating sample function

--- 04-functions.sh
+++ 04-functions.sh
@@ -14,8 +14,9 @@ sample_sum() {
 
 sample_sum $a $b #main
 
+#Passing variable as argument using special variable
 sample_special_var() {
-  echo Subtraction of $1 and $2 is $(($1+$2))
+  echo Subtraction of $1 and $2 is $(($1-$2))
 }
 
-sample_special_var $1 $2
+sample_special_var $1 $2 # 100 200
