65ec284451868b802a6bd6fab10dac4db5a19312
Sumanth <9ssumanth@gmail.com>
2024-05-06 08:12:39 +0530

    Special variables

--- 03-special-variables.sh
+++ 03-special-variables.sh
@@ -1,8 +1,8 @@
 #Special variables start from 0...n, *, #
 #For accessing variable prefix $
 echo print script name - $0
-echo First argument - $1
-echo Second argument - $2
+#echo First argument - $1
+#echo Second argument - $2
 echo Third argument - $3
 echo List all arguments - $*
 echo No. of arguments - $#
