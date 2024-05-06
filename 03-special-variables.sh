2bcd312ddeb6fa54f08dd279c0f80d0666bb2048
Sumanth <9ssumanth@gmail.com>
2024-05-06 08:12:13 +0530

    Special variables

--- 03-special-variables.sh
+++ 03-special-variables.sh
@@ -3,5 +3,6 @@
 echo print script name - $0
 echo First argument - $1
 echo Second argument - $2
-echo List all argument - $*
-echo No. of argument - $#
+echo Third argument - $3
+echo List all arguments - $*
+echo No. of arguments - $#
