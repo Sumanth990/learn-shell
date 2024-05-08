e48075ee5925ba796780e12ffc5dccab45bb7c46
Sumanth <9ssumanth@gmail.com>
2024-05-08 15:27:23 +0530

    if condition

--- 06-if.sh
+++ 06-if.sh
@@ -1,21 +1,8 @@
-if [ -z "$1" ]; then
-   echo input a is empty
-   exit 1
-fi
-if [ -z "$2" ]; then
-   echo input b is empty
-   exit 1
-fi
+#string check
+# = == != -z-> checks if var is empty.
 
-if [ "$1" == "$2" ];
-then
-   echo "a is equal to b"
-elif [ "$1" -gt "$2" ];
-then
-   echo "a is greater than b"
-elif [ "$1" -lt "$2" ];
-then
-   echo "a is less than b"
-else
-   echo "None of the condition met"
+a=$1
+
+if [ $a == b ]; then
+  echo Welcome to devops
 fi
