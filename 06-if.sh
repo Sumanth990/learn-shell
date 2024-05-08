80a4f04e072521162236e6e3384411f8f5c41d52
Sumanth <9ssumanth@gmail.com>
2024-05-08 16:08:26 +0530

    if condition

--- 06-if.sh
+++ 06-if.sh
@@ -1,14 +1,15 @@
 #string check
 # = == != -z-> checks if var is empty.
 
-shopt -s nocasematch
+shopt -s nocasematch #enables case-insensitive option
 
-a=$1
+input=$1
 if [ -z "$a" ]; then
   echo "Enter an input"
+  exit 1
 fi
 
 
-if [[ "$a" == devops ]] ; then
-  echo "Welcome to devops"
+if [ "$input" == devops ] ; then
+  echo "Welcome to Devops"
 fi
