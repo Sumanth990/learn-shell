e5ec1d9262559c2d7bff7f22cc8a759eb7c12860
Sumanth <9ssumanth@gmail.com>
2024-05-08 15:43:08 +0530

    if condition

--- 06-if.sh
+++ 06-if.sh
@@ -2,7 +2,11 @@
 # = == != -z-> checks if var is empty.
 
 a=$1
+if [ -z "$a" ]; then
+  echo -i "Enter an input"
+fi
+
 
 if [ "$a" == devops ]; then
-  echo Welcome to devops
+  echo "Welcome to devops"
 fi
