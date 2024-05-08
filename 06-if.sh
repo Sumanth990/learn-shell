c77d5ec3868786341218091917e5b82454898d6c
Sumanth <9ssumanth@gmail.com>
2024-05-08 12:04:16 +0530

    if condition

--- 06-if.sh
+++ 06-if.sh
@@ -1,12 +1,12 @@
-if [ -z “$1” || -z "$2"] then
+if [[ -z "$1" || -z "$2" ]]; then
    echo input is empty
-elif [ $1 == $2 ]
+elif [ "$1" == "$2" ];
 then
    echo "a is equal to b"
-elif [ $1 -gt $2 ]
+elif [ "$1" -gt "$2" ];
 then
    echo "a is greater than b"
-elif [ $1 -lt $2 ]
+elif [ "$1" -lt "$2" ];
 then
    echo "a is less than b"
 else
