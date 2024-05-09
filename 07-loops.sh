c5a9a7c493066567f4b1433a1ab88b5a0b64a6da
Sumanth <9ssumanth@gmail.com>
2024-05-09 16:20:34 +0530

    Case example

--- 07-loops.sh
+++ 07-loops.sh
@@ -1,39 +1,53 @@
-##while loop
-#syntax:
-#while [ expression ]; do
-#  commands
-#done
-
+###while loop
+##syntax:
+##while [ expression ]; do
+##  commands
+##done
+#
+##i=10
+##while [ "$i" -gt 0 ]; do
+##  echo "Hello world"
+##  i=$(("$i"-1))
+##done
+#
+##For loop
+##syntax
+##for somevar in values(value1. value2); do
+##  commands
+##done
+#
+##for course in Devops AWS AZURE GCP; do
+##  echo Welcome to course - ${course}
+##done
+#
+##echo "###Break example"
+##i=10
+##while [ "$i" -gt 0 ]; do
+##  echo "Hello world"
+##  i=$(("$i"-1))
+##  break
+##  echo "Hello"
+##done
+#
+#echo "###continue example"
 #i=10
 #while [ "$i" -gt 0 ]; do
-#  echo "Hello world"
+#  echo Hello, world
 #  i=$(("$i"-1))
+#  continue
+#  echo "hellllllllo"
 #done
 
-#For loop
-#syntax
-#for somevar in values(value1. value2); do
-#  commands
-#done
+course=$1
 
-#for course in Devops AWS AZURE GCP; do
-#  echo Welcome to course - ${course}
-#done
-
-#echo "###Break example"
-#i=10
-#while [ "$i" -gt 0 ]; do
-#  echo "Hello world"
-#  i=$(("$i"-1))
-#  break
-#  echo "Hello"
-#done
-
-echo "###continue example"
-i=10
-while [ "$i" -gt 0 ]; do
-  echo Hello, world
-  i=$(("$i"-1))
-  continue
-  echo "hellllllllo"
-done
+case $course in
+ AWS)
+   echo welcome to AWS training
+   ;;
+ Devops)
+   echo welcome to DevOps training
+   ;;
+*)
+   echo Unknow training
+   ;;
+esac
