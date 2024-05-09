97d3b54fde93bf31a3c5a1a73897280b287255a5
Sumanth <9ssumanth@gmail.com>
2024-05-09 16:13:04 +0530

    Loop control commands - break, continue

--- 07-loops.sh
+++ 07-loops.sh
@@ -4,11 +4,11 @@
 #  commands
 #done
 
-i=10
-while [ "$i" -gt 0 ]; do
-  echo "Hello world"
-  i=$(("$i"-1))
-done
+#i=10
+#while [ "$i" -gt 0 ]; do
+#  echo "Hello world"
+#  i=$(("$i"-1))
+#done
 
 #For loop
 #syntax
@@ -19,3 +19,21 @@ done
 #for course in Devops AWS AZURE GCP; do
 #  echo Welcome to course - ${course}
 #done
+
+echo Break example
+i=10
+while [ "$i" -gt 0 ]; do
+  echo "Hello world"
+  i=$(("$i"-1))
+  break
+  echo "Hello"
+done
+
+echo continue example
+i=10
+while [ "$i" -gt 0 ]; do
+  echo "Hi, HRU"
+  i=$(("$i"-1))
+  continue
+  echo "hellllllllo"
+done
