67022cf8e76fdbf59746f4c5eed0431931dbb7c9
Sumanth <9ssumanth@gmail.com>
2024-05-17 13:11:15 +0530

    if

--- 06-if.sh
+++ 06-if.sh
@@ -6,9 +6,10 @@ shopt -s nocasematch #enables case-insensitive option
 input=$1
 if [ -z "$1" ]; then
   echo "Enter an input"
-  exit # default is 0
+  echo $?
+  exit 1 # default is 0
 fi
 
-if [ $input == devops ] ; then
+if [ "$input" == devops ] ; then
   echo "Welcome to Devops"
 fi
