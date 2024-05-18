9e4e7792700ef908b159808122c3dc080277afec
Sumanth <9ssumanth@gmail.com>
2024-05-18 17:54:39 +0530

    for loop

--- 07-loops.sh
+++ 07-loops.sh
@@ -1,6 +1,11 @@
-#While loop
-i=5
-while [ $i -gt 0 ]; do
-  echo Hi world!!!
-  i=$(($i-1))
+##While loop
+#i=5
+#while [ $i -gt 0 ]; do
+#  echo Hi world!!!
+#  i=$(($i-1))
+#done
+
+#For loop
+for course in AWS ANSIBLE LINUX "SHELL SCRIPTING" K8S; do
+  echo Welcome to course - $course
 done
