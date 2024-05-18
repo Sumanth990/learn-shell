798b7af9e6b2d4a6b519e61e729c2ac541909591
Sumanth <9ssumanth@gmail.com>
2024-05-18 18:10:38 +0530

    case

--- 07-loops.sh
+++ 07-loops.sh
@@ -1,32 +1,46 @@
-echo while loop
-i=5
-while [ $i -gt 0 ]; do
-  echo Hi world!!!
-  i=$(($i-1))
-done
+#echo while loop
+#i=5
+#while [ $i -gt 0 ]; do
+#  echo Hi world!!!
+#  i=$(($i-1))
+#done
+#
+#echo For loop
+#for course in AWS ANSIBLE LINUX "SHELL SCRIPTING" K8S; do
+#  echo Welcome to course - ${course}
+#done
+#
+#echo Two loop control commands
+#
+#echo Break - Stops the loop.
+#i=5
+#while [ $i -gt 0 ]; do
+#  echo "Hello!!!!"
+#  i=$(("$i"-1))
+#  break
+#  echo "HEHEHEHEHEHE"
+#done
+#
+#echo continue - Will skip the next commands in the loop.
+#
+#i=5
+#while [ $i -gt 0 ]; do
+#  echo "Hello!!!!"
+#  i=$(("$i"-1))
+#  continue
+#  echo "HEHEHEHEHEHE"
+#done
 
-echo For loop
-for course in AWS ANSIBLE LINUX "SHELL SCRIPTING" K8S; do
-  echo Welcome to course - ${course}
-done
-
-echo Two loop control commands
-
-echo Break - Stops the loop.
-i=5
-while [ $i -gt 0 ]; do
-  echo "Hello!!!!"
-  i=$(("$i"-1))
-  break
-  echo "HEHEHEHEHEHE"
-done
-
-echo continue - Will skip the next commands in the loop.
-
-i=5
-while [ $i -gt 0 ]; do
-  echo "Hello!!!!"
-  i=$(("$i"-1))
-  continue
-  echo "HEHEHEHEHEHE"
-done
+echo case
+i=$1
+case $i in
+AWS)
+  echo Welcome to AWS course
+  ;;
+DevOps)
+  echo Welcome to DevOps course
+  ;;
+*)
+  echo Course details not available
+  ;;
+esac
